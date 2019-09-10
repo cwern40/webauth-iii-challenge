db2a4724eeab13e69ca95d20fec30cda0a663f13
cwern40 <chris.wernli@hotmail.com>
2019-09-10 17:43:03 -0600

    I created the migration file and created a table with a username, password, and department column

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/users.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
     }
   },
 
